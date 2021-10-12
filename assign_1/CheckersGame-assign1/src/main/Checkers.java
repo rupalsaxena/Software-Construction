3276dda808071be8a77959ee0eb939fac418c4cd
armsp <shantam.raj@uzh.ch>
2021-10-12 15:58:21 +0200

    adding code for input testing, validation and splitting (splitting is not fully complete)

--- assign_1/CheckersGame-assign1/src/main/Checkers.java
+++ assign_1/CheckersGame-assign1/src/main/Checkers.java
@@ -1,33 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.Arrays;
+
 
 public class Checkers {
     static Scanner scanner = new Scanner(System.in);
-    public static void main() throws Exception {
-        System.out.print("Welcome to Checkers Game! \n");
-
-        int[][] board = Board.init_board();
-        Board.display_board(board);
-        // Board.print_board(board); // for printing board array
-
+    static String currentPlayer = "Red";
+    static String playerTurn = "Red";
+    public static void main() {
         Checkers checkers = new Checkers();
-        Validation validate = new Validation();
-        String player;
+        System.out.print("Welcome to Checkers - a game by Shantam and Rupal\n");
 
-        for (int i = 1; i < 3; i++) { // TODO: increase loops later
-            if (i%2 == 0) player = "White";
-            else player = "Red";
-            for (int j = 1; j < 3; j++) { // TODO: increase loops later
-                String new_move = checkers.input_move(player);
-                Boolean validity = validate.is_valid_input(new_move);
-                if (validity == true) break;
-                else System.out.println("Invalid move!");
-            }
-        }
-    }
+    };
+//    public String input_moves(String player) {
+//            System.out.println("Input your move player " + player + ":");
+//            BufferedReader reader = new BufferedReader(
+//                new InputStreamReader(System.in));
+//        return reader.readLine();
+//    }
 
-    public String input_move(String player) {
+    public static String input_move(String player) {
         System.out.println("Input your move player " + player + ":");
         String input = scanner.nextLine();
+        int isCorrectPlayer = Checkers.playerTurnValidate(input);
+        if (isCorrectPlayer == 1) {
+            if (Checkers.currentPlayer == "Red") {
+                Checkers.playerTurn = "White";
+                Checkers.currentPlayer = "White";
+            } else {
+                Checkers.playerTurn = "Red";
+                Checkers.currentPlayer = "Red";
+            }
+        }
+        else {
+            Checkers.playerTurn = Checkers.currentPlayer;
+        }
+//        scanner.close();
         return input;
     }
+    public static int[] processInputMove(String move){
+        //use the mapper to map alphabets to numbers and numbers are numbers
+        //return an int array
+        //[a1]x[b2]
+        //we need to check with a regex if the input follows this pattern, else we cannot split it
+        //Once the regex match is a success, we split it
+        final String pattern = "\\[[a-h][1-8]\\]x\\[[a-h][1-8]\\]"; //[a-h][1-8]:[a-h][1-8]
+        boolean regex_full_match = move.matches(pattern);
+        if (regex_full_match==true){
+            //split and map the input and return them
+            System.out.print(regex_full_match+"\n");
+//            String[] split_move = move.split("x");
+//            System.out.print(Arrays.deepToString(split_move));
+            System.out.print("\n");
+            System.out.print(move.split(""));
+            System.out.print("\n");
+            char[] char_input = move.replaceAll("[\\[\\]x]","").toCharArray();
+            System.out.print(char_input[0]+" "+char_input[1]+" "+char_input[2]+" "+char_input[3]);
+            System.out.print("\n");
+            System.out.print(move.split("(?<=.)"));
+            System.out.print("\n");
+            //map characters to integers
+            return new int[] {1,2,3,4};
+        }
+        else{
+            System.out.print(regex_full_match+"\n");
+            return new int[] {0};
+            //Not a valid input
+            //How to show the prompt once again?
+        }
+    }
+    public static int playerTurnValidate(String inputMove){
+        //use input move and current player info to return 1 or 0
+        return 0;
+    }
+//    public static void makePawns() {
+//        for (i=0;i<30;i++){
+//            new Pawn();
+//        }
+//    };
+//    public static void makePlayers() {
+//
+//    };
 }
--- assign_1/CheckersGame-assign1/src/main/Launcher.java
+++ assign_1/CheckersGame-assign1/src/main/Launcher.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
 public class Launcher{
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Checkers.main();
+        int[][] board = Board.setBoard();
+        Board.printBoard(board);
+//        System.out.print(Arrays.deepToString(board));
+        while (true){
+            String player_move = Checkers.input_move(Checkers.playerTurn);
+            int[] mapped_move = Checkers.processInputMove(player_move);
+        }
     }
 }

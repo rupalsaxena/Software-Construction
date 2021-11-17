0c0aef8a311bc5e9b2d5f41d8303de8713ec4535
Dominik Sidler <Dominik.Sidler00@gmail.com>
2021-11-17 13:44:20 +0100

    Added folder for tests

---
+++ assign_3/question_2/src/tests/java/PieceTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PieceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void move_piece() {
+    }
+
+    @Test
+    void eliminate_piece() {
+    }
+
+    @Test
+    void encoded_value() {
+    }
+
+    @Test
+    void is_empty() {
+    }
+
+    @Test
+    void piece_equals() {
+    }
+
+    @Test
+    void is_valid_move() {
+    }
+
+    @Test
+    void registerObserver() {
+    }
+
+    @Test
+    void removeObserver() {
+    }
+
+    @Test
+    void notifyObservers() {
+    }
+}
